8a6660f28bf524a7232c385de5fe848bdaf2888a
KittyCancer <zkyle963@gmail.com>
2018-10-01 22:43:02 -0500

    Added regex and extraction of href into WikiCrawler

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
+        WikiCrawler wc = new WikiCrawler("0", 1, null, null);
     }
 }
--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -1,7 +1,12 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WikiCrawler {
-
+    static final String BASE_URL = "https://en.wikipedia.org";
     /**
      *
      * @param seed related address of seed URL (within wiki domain)
@@ -9,8 +14,19 @@ public class WikiCrawler {
      * @param topics arrays of strings representing keywords in a topic-list
      * @param output string representing the filename where the web graph over discovered pages are written
      */
-    public WikiCrawler(String seed, int max, String[] topics, String output) {
+    public WikiCrawler(String seed, int max, String[] topics, String output) throws FileNotFoundException {
+        File testDoc = new File(".\\wiki_Iowa_State_University.html");
+        Scanner sc = new Scanner(testDoc).useDelimiter("<p>");
+        // OG ReGex - "<\\s*a[^>]*>(.*?)<\\s*/\\s*a>"
+        Pattern p = Pattern.compile("<a[^>]+href=[\\\"']?([\\\"'>]+)[\\\"']?[^>]*>(.+?)<\\/a>",  Pattern.CASE_INSENSITIVE|Pattern.DOTALL);
 
+        while(sc.hasNextLine()){
+            String line = sc.nextLine();
+            Matcher m = p.matcher(line);
+            while(m.find()){
+                System.out.println(m.group(0));
+            }
+        }
     }
 
     /**
