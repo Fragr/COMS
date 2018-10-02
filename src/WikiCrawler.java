dffbc8e233bcd69d5e3f22fa77923afe258dedcd
Unknown <p.j.debisschop@gmail.com>
2018-10-01 20:46:29 -0500

    Updated WikiCrawler

--- src/WikiCrawler.java
+++ src/WikiCrawler.java
@@ -1,2 +1,37 @@
+import java.util.ArrayList;
+
 public class WikiCrawler {
+
+    /**
+     *
+     * @param seed related address of seed URL (within wiki domain)
+     * @param max maximum number of pages to consider
+     * @param topics arrays of strings representing keywords in a topic-list
+     * @param output string representing the filename where the web graph over discovered pages are written
+     */
+    public WikiCrawler(String seed, int max, String[] topics, String output) {
+
+    }
+
+    /**
+     * Takes as input a document representing an entire HTML document.
+     * Returns a list of strings consisting of links from the document.
+     * @param document
+     * @return
+     */
+    private ArrayList<String> extractLinks(String document) {
+        return null;
+    }
+
+    /**
+     * Crawls/explores the web pages starting from the seed URL. Crawl the first max
+     * number of pages (including the seed page), that contains every keywords in\
+     * the Topics list (if Topics list is empty then this condition is vacuously considered true),
+     * and are explored starting from the seed.
+     * @param focused
+     * @return
+     */
+    private int crawl(boolean focused) {
+        return 0;
+    }
 }
