af51b5a66e60f5ad5f300c97d44337ad87fa44b3
Unknown <p.j.debisschop@gmail.com>
2018-10-09 22:39:28 -0500

    Added to WikiCrawler

--- src/KyleTest.java
+++ src/KyleTest.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 public class KyleTest {
 
     public static void test() throws IOException {
-        WikiCrawler wc = new WikiCrawler("1", 0, null, null);
+        String[] test = {"known"};
+        WikiCrawler wc = new WikiCrawler("/wiki/70th_Infantry_Division_(United_Kingdom)", 1, test, null);
+        wc.crawl(true);
         PriorityQ pq = new PriorityQ();
     }
 }
