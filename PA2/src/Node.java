8685ec2a5c580665145c2b6b4be8d27d8a99e51a
Unknown <p.j.debisschop@gmail.com>
2018-11-06 10:01:53 -0600

    Added a Node class

---
+++ PA2/src/Node.java
@@ -0,0 +1,24 @@
+public class Node {
+
+    private int X;
+    private int Y;
+
+    public Node(int x, int y) {
+        X = x;
+        Y = y;
+    }
+
+    /**
+     * @return X coordinate of this node
+     */
+    public int getX() {
+        return X;
+    }
+
+    /**
+     * @return Y coordinate of this node
+     */
+    public int getY() {
+        return Y;
+    }
+}
